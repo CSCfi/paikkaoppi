ff96e6e87c11adc06bae9de41a08aa5f95496ef5
perttvil <vili.perttila@affecto.com>
2017-09-12 11:30:35 +0200

    Added stable task Codes.

--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -6,21 +6,15 @@ import { TaskTemplateService } from './task-template.service'
 @Injectable()
 export class TaskService {
   tasks: Task[] = []
+  // Remove this when real api is in use and you dont have to create taskCodes in UI
+  private codeCreator: TaskCodeCreator = new TaskCodeCreator(new StableRandom(1))
 
   constructor(private taskTemplateService: TaskTemplateService) {
-    taskTemplateService.getTaskTemplates().then( t => this.tasks = t.map(this.toTask))
+    taskTemplateService.getTaskTemplates().then(t => this.tasks = t.map(template => this.toTask(template, this.codeCreator)))
   }
 
-  private toTask(template: TaskTemplate): Task {
-    function createCode(): string {
-      var text: string = "";
-      var possible: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-      for (var i = 0; i < 6; i++)
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-      return text;
-    }
-    let code: string = createCode()
+  private toTask(template: TaskTemplate, codeCreator: TaskCodeCreator): Task {
+    let code: string = codeCreator.createCode()
     return {
       id: template.id,
       name: template.name,
@@ -38,16 +32,42 @@ export class TaskService {
   }
 
   getTask(id: number): Promise<Task> {
-    const task = this.tasks.find( t => t.id == id)
+    const task = this.tasks.find(t => t.id == id)
     return Promise.resolve(task)
   }
 
-  createTaskFrom(id: number, name: string) : Promise<Task> {
-    return this.taskTemplateService.getTaskTemplate(id).then( template => {
-      let task = this.toTask(template)
+  createTaskFrom(id: number, name: string): Promise<Task> {
+    return this.taskTemplateService.getTaskTemplate(id).then(template => {
+      let task = this.toTask(template, this.codeCreator)
       task.name = name
+      task.id = this.tasks.length + 1
       this.tasks.push(task)
       return task
     })
+  }
+}
+
+// Remove this when real api is in use and you dont have to create taskCodes in UI
+class StableRandom {
+  constructor(private seed: number) {
+  }
+
+  random(): number {
+    const x = Math.sin(this.seed++) * 10000
+    return x - Math.floor(x)
+  }
+}
+
+// Remove this when real api is in use and you dont have to create taskCodes in UI
+class TaskCodeCreator {
+  constructor(private random: StableRandom) { }
+
+  createCode() {
+    var text: string = "";
+    var possible: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    for (var i = 0; i < 6; i++)
+      text += possible.charAt(Math.floor(this.random.random() * possible.length))
+    return text
   }
 }
