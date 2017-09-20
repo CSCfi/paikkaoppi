da833c0d21af81b8899d9a0f44063ea64e7f9222
perttvil <vili.perttila@affecto.com>
2017-09-20 14:31:30 +0300

    Fixed proto script.

--- deploy_proto.sh
+++ deploy_proto.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-function build {
-    echo "Building app..."
-    eval "ng build --target=production --environment=prod"
-}
-
 function deploy {
     echo "Deploy app to S3."
     eval "aws s3 cp --recursive --profile paikkaoppi-deploy ./proto s3://demo-paikkaoppi/proto/"
 }
 
-build && deploy
+deploy
 echo "Done."
 exit
