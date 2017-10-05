634272521f558f4075a7e3faf00a572f32da4e34
Markus Kettunen (Weave) <markus.kettunen@weave.fi>
2017-10-05 15:22:41 +0300

    Removed e2e test that needed API to be up and running

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,31 +1,16 @@
 import { browser, by, element } from 'protractor'
 
-import { LoginPage, MainPage } from './app.po'
+import { LoginPage } from './app.po'
 
 describe('Paikkaoppi App', () => {
   let loginPage: LoginPage
-  let mainPage: MainPage
 
   beforeEach(() => {
     loginPage = new LoginPage()
-    mainPage = new MainPage()
   })
 
   it('should display login button', () => {
     loginPage.navigateTo()
     expect(loginPage.getLoginLink().getText()).toContain('kaisa')
-  })
-
-  it('should allow login and redirect to main page', () => {
-    browser.driver.sleep(500)
-
-    loginPage.getLoginLink().click()
-    browser.driver.sleep(500)
-
-    console.log(element(by.css('app-root')).getText())
-
-    expect(mainPage.getItemTitleText()).toEqual('Lataa tehtävä')
-
-    browser.driver.sleep(500)
   })
 })
