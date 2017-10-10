cbce756f05fc95157325b3e0d15e396261114d4f
perttvil <vili.perttila@affecto.com>
2017-10-10 12:25:42 +0300

    removed unused

--- src/app/service/model.ts
+++ src/app/service/model.ts
@@ -100,11 +100,3 @@ export class TaskCodeCreator {
         return text
     }
 }
-
-export class Sequence {
-    value = 0
-    next(): number {
-        this.value = this.value + 1
-        return this.value
-    }
-}
