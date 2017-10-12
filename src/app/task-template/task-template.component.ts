6ae50b32a11c6d95f12751594d86841a4d58090c
perttvil <vili.perttila@affecto.com>
2017-10-12 14:55:45 +0300

    Added more implementation. Instruction binding missing.

--- src/app/task-template/task-template.component.ts
+++ src/app/task-template/task-template.component.ts
@@ -19,22 +19,33 @@ export class TaskTemplateComponent implements OnInit {
   ngOnInit() {
     console.log('ngOnInit')
     this.model = this.createNewModel()
-    this.phase = 1
+    // TODO: VILI Change this to 1
+    this.phase = 2
   }
 
   closeDialog() {
-    console.log('closeDialog')
+    console.log('closeDialog', this.model)
     this.close.next()
   }
 
   submit() {
-    console.log('submit')
+    console.log('submit', this.model)
     this.save.next(this.model)
   }
 
   next() {
-    console.log('next')
+    console.log('next', this.model)
     this.phase++
+  }
+
+  addInstruction(): void {
+    this.model.instructions.push(this.createNewInstruction())
+    console.log('addInstruction', this.model)
+  }
+
+  removeInstruction(instruction: Instruction): void {
+    this.model.instructions = this.model.instructions.filter( i => i !== instruction)
+    console.log('removeInstruction', this.model)
   }
 
   private createNewModel(): any {
@@ -42,9 +53,16 @@ export class TaskTemplateComponent implements OnInit {
       name: null,
       title: null,
       description: null,
-      instructions: [],
+      instructions: [this.createNewInstruction()],
       info: null,
       tags: []
+    }
+  }
+
+  private createNewInstruction(): Instruction {
+    return <Instruction>{
+      name: null,
+      description: null
     }
   }
 }
