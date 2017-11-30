a998141b6cafdd61430f55531ec955438e969e84
Markus Kettunen (Weave) <markus.kettunen@weave.fi>
2017-11-30 13:20:33 +0200

    Deploy to csc fixes

--- deploy_test.sh
+++ deploy_test.sh
@@ -11,11 +11,11 @@ function build {
 function deploy {
     echo "Deploy app to server."
     
-    # send to test server. You need to setup 'paikkaoppi-test-apache' to ssh/config
-    eval "scp -r dist paikkaoppi-test-apache:"
+    # send to test server. You need to setup 'paikkaoppi-dev-front' to ssh/config
+    eval "scp -r dist paikkaoppi-dev-front:"
 
     #set jar as executable, move it to /opt/paikkaoppi/, restart service
-    eval "ssh paikkaoppi-test-apache 'sudo rm -rf /opt/paikkaoppi/static/app/*; sudo cp -R ~/dist/* /opt/paikkaoppi/static/app/; sudo chown -R root:root /opt/paikkaoppi/static/app/; sudo rm -rf ~/dist/*;'"
+    eval "ssh paikkaoppi-dev-front 'sudo rm -rf /opt/paikkaoppi/static/app/*; sudo cp -R ~/dist/* /opt/paikkaoppi/static/app/; sudo chown -R root:root /opt/paikkaoppi/static/app/; sudo rm -rf ~/dist/*;'"
 }
 
 build && deploy
